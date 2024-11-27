0e336aa66e1c869906f62b156dea394de9c18782
江夏尧 <3446798488@qq.com>
2024-11-27 15:50:05 +0000

    fix: fix svg c code extern problem

--- src/subset/hb-glyph-to-svg-path.h
+++ src/subset/hb-glyph-to-svg-path.h
@@ -1,3 +1,6 @@
 #include "../../harfbuzz/src/hb.h"
 
-int hb_glyph_to_svg_path(hb_font_t *font, hb_codepoint_t glyph, char *buf, unsigned buf_size);
+extern "C"
+{
+    int hb_glyph_to_svg_path(hb_font_t *font, hb_codepoint_t glyph, char *buf, unsigned buf_size);
+}
