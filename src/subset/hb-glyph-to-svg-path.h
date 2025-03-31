82b78af56b3ab97b35992c9c06915375f66ab2c8
江夏尧 <3446798488@qq.com>
2025-03-31 22:17:42 +0800

    refactor: 更新C++兼容性并升级依赖版本
    在hb-glyph-to-svg-path.h中添加了C++兼容性保护宏，确保在C++环境中正确使用extern "C"。同时，更新了Cargo.toml中的版本号和依赖项，将cc依赖从1.2.1升级到1.2.17，并优化了作者列表的格式。

--- src/subset/hb-glyph-to-svg-path.h
+++ src/subset/hb-glyph-to-svg-path.h
@@ -1,6 +1,12 @@
 #include "../../harfbuzz/src/hb.h"
 
+#ifdef __cplusplus
 extern "C"
 {
+#endif
+
     int hb_glyph_to_svg_path(hb_font_t *font, hb_codepoint_t glyph, char *buf, unsigned buf_size);
+
+#ifdef __cplusplus
 }
+#endif
